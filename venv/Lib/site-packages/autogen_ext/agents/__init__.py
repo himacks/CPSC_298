abf597a87a14a438cff950f9a01fa5e3d5ab4c58
himacks <karpinskymaxim@gmail.com>
2024-12-11 21:53:52 -0800

    Remove venv from git tracking

--- venv/Lib/site-packages/autogen_ext/agents/__init__.py
+++
@@ -1,3 +0,0 @@
-from .web_surfer._multimodal_web_surfer import MultimodalWebSurfer
-
-__all__ = ["MultimodalWebSurfer"]
--- venv/Lib/site-packages/autogen_ext/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._openai._openai_client import (
-    AzureOpenAIChatCompletionClient,
-    OpenAIChatCompletionClient,
-)
-
-__all__ = [
-    "AzureOpenAIChatCompletionClient",
-    "OpenAIChatCompletionClient",
-]
